011c8ca061c399bbb1c13821d8ad96395979145e
marcos <marcos123_batista@hotmai.com>
2026-03-18 20:29:41 -0300

    Primeiro Commit

---
+++ src/main/java/RepositoryLivros/RepositoryLivros.java
@@ -0,0 +1,9 @@
+package RepositoryLivros;
+
+import ModelLivros.ModelLivros;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositoryLivros extends JpaRepository<ModelLivros, Long> {
+}
---
+++ src/main/java/ServiceLivros/ServiceLivros.java
@@ -0,0 +1,35 @@
+package ServiceLivros;
+
+import ModelLivros.ModelLivros;
+import RepositoryLivros.RepositoryLivros;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ServiceLivros {
+    private final RepositoryLivros repositoryLivros;
+
+    public ServiceLivros(RepositoryLivros repositoryLivros) {
+        this.repositoryLivros = repositoryLivros;
+    }
+
+    public void criar (ModelLivros modelLivros){
+        repositoryLivros.save(modelLivros);
+    }
+
+    public List<ModelLivros> listar() {
+        return repositoryLivros.findAll();
+    }
+
+    public ModelLivros buscarLivro(Long id) {
+        return repositoryLivros.findById(id).orElseThrow(() -> new RuntimeException("Livro com ID " + id + " não encontrado"));
+    }
+
+    public void remover (Long id){
+        if (repositoryLivros.existsById(id)) {
+            repositoryLivros.deleteById(id);
+        } else {
+            throw new RuntimeException("ID" + id + "nao encontrado");
+        }
+    }
+}
