e216beba8268575837412f9ad66fa1e3bda1cd6a
Marcos Vinicius de Azevedo Batista <marcos123_batista@hotmail.com>
2026-03-20 21:44:36 -0300

    salvando second branch

--- src/main/java/RepositoryLivros/RepositoryLivros.java
+++
@@ -1,9 +0,0 @@
-package RepositoryLivros;
-
-import ModelLivros.ModelLivros;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryLivros extends JpaRepository<ModelLivros, Long> {
-}
