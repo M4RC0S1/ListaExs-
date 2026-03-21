c95a375ea6116d053c653545302f89fb50e7e3f8
Marcos Vinicius de Azevedo Batista <marcos123_batista@hotmail.com>
2026-03-21 18:53:11 -0300

    exercicio 05

--- src/main/java/RepositoryAutor/RepositoryAutor.java
+++
@@ -1,9 +0,0 @@
-package RepositoryAutor;
-
-import ModelAutor.ModelAutor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryAutor extends JpaRepository<ModelAutor, Long> {
-}
