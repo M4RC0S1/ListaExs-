87968c3b973a0357ffb849066e71b43c2437d78c
Marcos Vinicius de Azevedo Batista <marcos123_batista@hotmail.com>
2026-03-21 18:25:52 -0300

    exercicio 4

--- src/main/java/RepositoryPedido/RepositoryPedido.java
+++
@@ -1,9 +0,0 @@
-package RepositoryPedido;
-
-import ModelPedido.ModelPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class RepositoryPedido extends JpaRepository<ModelPedido, Long> {
-}
