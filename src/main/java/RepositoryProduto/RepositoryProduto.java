336ac07659fc564229d3a103ba9be328770a05cd
Marcos Vinicius de Azevedo Batista <marcos123_batista@hotmail.com>
2026-03-21 11:48:28 -0300

    salvando a terceira branch

--- src/main/java/RepositoryProduto/RepositoryProduto.java
+++
@@ -1,9 +0,0 @@
-package RepositoryProduto;
-
-import ModelProduto.ModelProduto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryProduto extends JpaRepository<ModelProduto, Long> {
-}
