e4af26179880639e78e01de8b5eb5f4a4feb4eb7
Marcos Vinicius de Azevedo Batista <marcos123_batista@hotmail.com>
2026-03-22 11:07:01 -0300

    exercicio 08

--- src/main/java/RepositoryFornecedor/RepositoryFornecedor.java
+++
@@ -1,9 +0,0 @@
-package RepositoryFornecedor;
-
-import ModelFornecedor.ModelFornecedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryFornecedor extends JpaRepository <ModelFornecedor, Long> {
-}
