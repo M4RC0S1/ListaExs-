88e3830647b3f0bf52abd758503a590e09bb810d
Marcos Vinicius de Azevedo Batista <marcos123_batista@hotmail.com>
2026-03-22 11:19:05 -0300

    att

---
+++ src/main/java/ServiceDepartamento/ServiceDepartamento.java
@@ -0,0 +1,36 @@
+package ServiceDepartamento;
+
+import ModelDepartamento.ModelDepartamento;
+import RepositoryDepartamento.RepositoryDepartamento;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceDepartamento {
+    private final RepositoryDepartamento repositoryDepartamento;
+
+    public ServiceDepartamento(RepositoryDepartamento repositoryDepartamento){
+        this.repositoryDepartamento = repositoryDepartamento;
+    }
+
+    public void criar(ModelDepartamento modelDepartamento){
+        repositoryDepartamento.save(modelDepartamento);
+    }
+
+    public List<ModelDepartamento> listar(){
+        return repositoryDepartamento.findAll();
+    }
+
+    public ModelDepartamento buscarCat(Long id){
+        return repositoryDepartamento.findById(id).orElseThrow(()-> new RuntimeException("Departamento com ID " + id + "não encontrado"));
+    }
+
+    public void remover(Long id){
+        if (repositoryDepartamento.existsById(id)){
+            repositoryDepartamento.deleteById(id);
+        } else {
+            throw new RuntimeException("ID" + id + "nao encontrado");
+        }
+    }
+}
